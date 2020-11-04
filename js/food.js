90974fdf5b313785f545a9580fc70dc978de2dec
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-04 10:31:08 +0700

    lab s13 - step 2 3 + food

---
+++ js/food.js
@@ -0,0 +1,104 @@
+var data = {
+    categories: [
+        {
+            name: "Vegetables"
+        },
+        {
+            name: "Fresh fruit"
+        },
+        {
+            name: "Vegetables"
+        },
+        {
+            name: "Fresh fruit"
+        },
+        {
+            name: "Vegetables"
+        },
+        {
+            name: "Fresh fruit"
+        }
+    ],
+    brands:[
+        {
+            name: "Green life"
+        },
+        {
+            name: "Organic life"
+        },
+        {
+            name: "Green life"
+        },
+        {
+            name: "Organic life"
+        },
+        {
+            name: "Green life"
+        },
+        {
+            name: "Organic life"
+        }
+    ],
+    products: [
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        },
+        {
+            name:"Flying Ninja",
+            image:"imgs/product.jpg",
+            price: 12,
+            star:3.5
+        }
+    ]
+}
