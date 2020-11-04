3e78c49dcea8e1b3218c30314389d852a67641f5
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-04 08:50:19 +0700

    lab s13

---
+++ js/grid.js
@@ -0,0 +1,63 @@
+var products = [
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    },
+    {
+        name:"Flying Ninja",
+        image:"imgs/product.jpg",
+        price: 12,
+        star:3.5
+    }
+
+]
--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,36 @@
+var cats = [
+    {
+        name:"Quần áo",
+        url:"#"
+    },
+    {
+        name:"Giày dép",
+        url:"#"
+    },
+    {
+        name:"Đồng hồ",
+        url:"#"
+    },
+    {
+        name:"Trang sức",
+        url:"#"
+    },
+    {
+        name:"Kim cương",
+        url:"#"
+    }
+];
 function nav() {
-    return "<div class=\"container\">\n" +
-        "            <ul class=\"menu\">\n" +
-        "                <li><a href=\"#\">Category #1</a></li>\n" +
-        "                <li><a href=\"#\">Category #2</a></li>\n" +
-        "                <li><a href=\"#\">Category #3</a></li>\n" +
-        "                <li><a href=\"#\">Category #4</a></li>\n" +
-        "                <li class=\"last-item\"><a href=\"#\">Get Genesis Now!</a></li>\n" +
+    var html_nav = "<div class=\"container\">\n" +
+        "            <ul class=\"menu\">\n" ;
+    // can nap cac category vao
+    for(var i=0;i<cats.length;i++){ // can thay cats[i].name
+        html_nav+= "<li><a href=\""+cats[i].url+"\">"+cats[i].name+"</a></li>\n";
+    }
+    html_nav+=    "                <li class=\"last-item\"><a href=\"#\">Get Genesis Now!</a></li>\n" +
         "            </ul>\n" +
-        "        </div>"
+        "        </div>";
+    return html_nav;
 }
 var na = document.getElementById("nav");
 na.innerHTML = nav();
